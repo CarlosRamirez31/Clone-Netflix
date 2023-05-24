9302661c0638c00608bd98c5a86a387b38575edb
Carlos <cr2503150@gmail.com>
2023-05-23 23:27:12 -0400

    adding functions to insert images in the carousel

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 const BASE_URL = "https://api.themoviedb.org/3";
-const TOKEN = "Youu Token";
+const TOKEN = "you token";
 const IMG_URL = "https://image.tmdb.org/t/p/w500"
 const preview_IMG = document.getElementById("preview_IMG");
 const movie = document.querySelector(".hero");
+const carousel1 = document.getElementById("carousel1");
+const carousel2 = document.getElementById("carousel2");
+const next = document.getElementById("next");
+
+let count = 1;
+
+next.addEventListener("click", () => {
+  count++;
+  if(count < 3) carousel();
+})
+
 
 const options = {
   method: 'GET',
@@ -24,11 +35,43 @@ async function preview(){
                       <p>${data.overview}</p>
                     </div>
                     <div class="movie">
-                      <img src="${IMG_URL + data.backdrop_path}" alt="Imagen">
+                      <img src="${IMG_URL + data.backdrop_path}" alt="${data.title}">
                     <div>
                   `
 
   movie.insertAdjacentHTML("beforeend", element);
 }
 
-preview();
+function insertImgs(data){
+  let element = '';
+  let contElement = 1;
+
+
+  data.results.forEach(pelicula => {
+    if(contElement < 7)
+    { 
+      element +=`
+                  <div class="col-2 ">
+                      <img class="img-fluid" src="${IMG_URL + pelicula.backdrop_path}" alt="${pelicula.title}">
+                  </div>
+                `;
+    }
+    contElement++;
+  });
+
+  return element;
+}
+
+async function carousel(){
+  const url = `${BASE_URL}/movie/popular?language=es&page=${count}`;
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  if(count == 1){
+    carousel1.insertAdjacentHTML("beforeend", insertImgs(data));
+  }
+  else{
+    carousel2.insertAdjacentHTML("beforeend", insertImgs(data));
+  }
+    
+}
